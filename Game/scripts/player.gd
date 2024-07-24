56be4fbf285cffe3986059a080e31d7aa040e206
Vincent Zheng <vzheng147@gmail.com>
2024-07-24 10:45:51 -0400

    optimized attack

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_attacking = false
 
 # Player Stats
 var attack = 5
@@ -45,7 +46,15 @@ func die():
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-
+func _input(event):
+	if event.is_action_pressed("Attack"):
+		is_attacking = true
+		print("ATTACK")
+		animated_sprite.play("attack")
+		await animated_sprite.animation_finished
+		is_attacking = false
+		
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -63,15 +72,15 @@ func _physics_process(delta):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-		
-	# Play animations 
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")	
+	
+	if (!is_attacking):
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")	
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			animated_sprite.play("jump")
 		
 	#Apply movement
 	if direction:
